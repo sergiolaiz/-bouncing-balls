67f9bbe0e8b76ff8d7833d920b6b23dc4e44b977
unknown <sergio.laiz87@gmail.com>
2019-06-01 14:25:48 +0200

    Permite crear bolas en posicion y tamaños aleatorios

--- BallDemo.java
+++ BallDemo.java
@@ -1,5 +1,5 @@
 import java.awt.Color;
-
+import java.util.ArrayList;
 /**
  * Class BallDemo - a short demonstration showing animation with the 
  * Canvas class. 
@@ -11,6 +11,7 @@ import java.awt.Color;
 public class BallDemo   
 {
     private Canvas myCanvas;
+    private ArrayList<BouncingBall> bolas;
 
     /**
      * Create a BallDemo object. Creates a fresh canvas and makes it visible.
@@ -18,35 +19,42 @@ public class BallDemo
     public BallDemo()
     {
         myCanvas = new Canvas("Ball Demo", 600, 500);
+        bolas = new ArrayList<BouncingBall>();
     }
 
     /**
      * Simulate two bouncing balls
      */
-    public void bounce()
+    public void bounce(int numBolas)
     {
         int ground = 400;   // position of the ground line
 
         myCanvas.setVisible(true);
 
+        bolas = new ArrayList<BouncingBall>();
         // draw the ground
         myCanvas.drawLine(50, ground, 550, ground);
 
-        // crate and show the balls
-        BouncingBall ball = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
-        ball.draw();
-        BouncingBall ball2 = new BouncingBall(70, 80, 20, Color.RED, ground, myCanvas);
-        ball2.draw();
+        for (int cont = 0; cont < numBolas; cont ++){
+            int diametroBola = (int) (Math.random()*100+5);
+            int posX = (int) (Math.random()*50+5);
+            int posy = (int) (Math.random()*50+5);
+            BouncingBall ball = new BouncingBall(posX, posy, diametroBola, Color.BLUE, ground, myCanvas);
+            ball.draw();
+            bolas.add(ball);
+        }
 
         // make them bounce
         boolean finished =  false;
         while(!finished) {
             myCanvas.wait(50);           // small delay
-            ball.move();
-            ball2.move();
-            // stop once ball has travelled a certain distance on x axis
-            if(ball.getXPosition() >= 550 || ball2.getXPosition() >= 550) {
-                finished = true;
+            //Recoremos el array de bolas con un bucle para ir moviendo cada bola.
+            for(BouncingBall bolaTem : bolas){
+                bolaTem.move();
+                // stop once ball has travelled a certain distance on x axis
+                if(bolaTem.getXPosition() >= 550 ) {
+                    finished = true;
+                }
             }
         }
     }
